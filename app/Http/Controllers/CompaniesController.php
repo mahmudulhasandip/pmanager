626d4b31cc93971935f2003611e9a38b91e27fa6
Md. Mahmudul Hasan Dip <ablaze.dip@gmail.com>
2018-01-23 18:28:24 +0600

    Companies show for particular user

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -29,7 +29,7 @@ class CompaniesController extends Controller
     public function index()
     {
         //
-        $companies = Company::all();
+        $companies = Company::where('user_id', Auth::user()->id)->get();
         return view('companies.index', ['companies'=>$companies]);
     }
 
